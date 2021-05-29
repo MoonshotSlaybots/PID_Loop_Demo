a60b7e16f8c79e6335a57dfc26bb780aae8be801
crglick <crglick@iastate.edu>
2021-05-29 16:00:51 -0500

    wrote basic sketch to read value from a potentiometer

---
+++ basic_pot_sketch/basic_pot_sketch.ino
@@ -0,0 +1,19 @@
+/**
+ * Reads the value of a potentiometer connected to analog pin 2
+ * and prints it out to the serial port 10 times per second
+ */
+
+int potPin = 2;   //input pin for the potentiometer
+int val = 0;      //current value of the potentiometer
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  val = analogRead(potPin);     //read value from pot
+  Serial.print(val);            //print value to serial
+  Serial.print("\n");
+  delay(100);
+}
