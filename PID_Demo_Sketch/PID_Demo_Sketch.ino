af6d30c916cd9b9db84db90f5e051f642cfb4b36
crglick <crglick@iastate.edu>
2021-07-25 00:07:07 -0500

    added filtering to analog inputs and other work

--- PID_Demo_Sketch/PID_Demo_Sketch.ino
+++ PID_Demo_Sketch/PID_Demo_Sketch.ino
@@ -1,24 +1,32 @@
 #include <Servo.h>
 
 //Constants
-const int setPositionPot = 3;
-const int currPositionEncoder = 4;
-const int Ppot = 0;
-const int Ipot = 1;
-const int Dpot = 2;
+const int setPosPot = A3;
+const int currPosEncoder = A4;
+const int Ppot = A0;
+const int Ipot = A1;
+const int Dpot = A2;
 
-const int invertSetPositionSwitch = 3;
+const int invertSetPosSwitch = 3;
 const int enableMotorSwitch = 4;
+const int servoPin = 5;
+
+const int pEnablePin = 6;
 
 const int maxPWM = 1750;  //in microseconds, the duty cycle PWM range to control speed
 const int minPWM = 1250;  //this is specific to the HSR-1425CR servo, tweak as needed for others
-const int servoPin = 2;
 
 //calculation variables
-float setPosition, currPosition, pIn, iIn, dIn = 0;   //inputs
-long error, lastError, pid, now = 0;
-long p, i, d = 0;       //values for p, i, and d factors
-long Kp, Ki, Kd = 0;    //coefficents 
+float setPos, currPos, pIn, iIn, dIn = 0.0f;   //analog inputs
+bool enableMotor, invertSetPos, pEnable = false;     //switch inputs
+float error, lastError, pid = 0.0f;    //TODO: label
+float p, i, d = 0.0f;       //values for p, i, and d factors
+float Kp, Ki, Kd = 0.0f;    //coefficents 
+
+//last interation of inputs for filtering
+float lastSetPos, lastCurrPos, lastPIn, lastIIn, lastDIn = 0.0f;
+
+long now = 0L;
 
 Servo servo;
 
@@ -27,14 +35,15 @@ Servo servo;
 */
 void setup() {
   Serial.begin(9600);   //start serial communication for console
+  now = millis() + 500;
   
   //input setup
-  pinMode(invertSetPositionSwitch, INPUT);
+  pinMode(invertSetPosSwitch, INPUT);
   pinMode(enableMotorSwitch, INPUT);
+  pinMode(pEnablePin, INPUT_PULLUP);
 
   //motor setup
   servo.attach(servoPin);
-
 }
 
 
@@ -42,139 +51,186 @@ void setup() {
  * Main loop that controls the motor
  */
 void loop() {
-  //first read all the inputs into the system
-  pIn = analogRead(Ppot);   //range 0 to 1024
-  iIn = analogRead(Ipot);
-  dIn = analogRead(Dpot);
-
-  //normally range 0 to 1024, make range -512 to +511
-  setPosition = (analogRead(setPositionPot)/512.0 * 180.0 ) - 180;
-  currPosition = readAnalogAngle(currPositionEncoder, 5);
+  Serial.println(digitalRead(pEnablePin) == HIGH);
   
-  //invert setPosition if needed
-  if(digitalRead(invertSetPositionSwitch) == HIGH){
-    //TODO: fix inversion, instead of 1 to -1, needs to be 1 to -179 degrees
-    setPosition = setPosition * -1;
+  //set last values
+  lastPIn = pIn;
+  lastIIn = iIn;
+  lastDIn = dIn;
+  lastCurrPos = currPos;  
+  
+  //first read all the inputs into the system
+  pIn = analogReadFiltered(Ppot, lastPIn, 0.9);   //range 0 to 1024
+  iIn = analogReadFiltered(Ipot, lastIIn, 0.9);
+  dIn = analogReadFiltered(Dpot, lastDIn, 0.9);
+  setPos = analogReadFiltered(setPosPot, lastSetPos, 0.9);
+  
+  lastSetPos = setPos;  //TODO: clean this up, order things better and comment
+
+  //setPos = analogRead(setPosPot);
+  
+  enableMotor = digitalRead(enableMotorSwitch) == HIGH;
+  invertSetPos = digitalRead(invertSetPosSwitch) == HIGH;
+
+  //read set and current position 
+  //normal range int 0 to 1024, make range float -180 to 180
+  setPos = ((setPos / 1024.0) * 360) - 180;
+  currPos = readAnalogAngle(currPosEncoder, lastCurrPos, 0.9);
+
+
+  //invert setPos if needed
+  if(invertSetPos){
+    //turn 180 degrees depending on the current state of the set position
+    if(setPos > 0){
+      setPos -= 180;
+    }else{
+      setPos += 180;
+    }
   }
   
-
   //begin PID loop calculations
 
-  //Create and Scale the coefficients 
+  //Scale the coefficients before they are passed in
   //from the input values (trial and error for scaling)
-  Kp = pIn / 4;      // 0 to 255
-  Ki = iIn / 4;     // 0 to 255
-  Kd = dIn ;      // 0 to 1023
+  Kp = pIn / 4096.0f;     // 0 to 1
+  Ki = iIn / 16384.0f;     // 0 to 0.5
+  Kd = dIn / 4096.0f ;    // 0 to 1
   
-  error = currPosition - setPosition;    //error is differene between where we want to be and where we are
+  error = currPos - setPos;    //error is degrees difference between where we want to be and where we are
 
-  p = error;              //proportional is equal to the amount of error
+  p = error;              //proportional is equal to the amount of error measured
 
-  i = i + error;         //integreal accumulates the error each iteration
+  //i = i + error;         //integral accumulates the error each iteration
   
   //Optional: create a dead band so the so integrel won't hunt back and fourth
-  //if(abs(error) >  1) i = i + error;  // Integrate error if error > 1
-  //if(error == 0) i = 0;               //Clear intergal if zero error
+  if(abs(error) >  5) i = i + error;  // Integrate error if error > 1
+  if(error < 5.0 && error > -5.0) i = 0;               //Clear intergal if zero error
 
-  i = constrain(i, -2000, 2000);      //stop i from increasing without bound
+  i = constrain(i, -500.0f, 500.0f);      //stop i from increasing without bound
 
-  d = error - lastError;              //rate of change of the error
+  d = error - lastError;              //derivitive is the rate of change of the error
   lastError = error;                  //save this error for the next loop
 
   //sum the PID factors with their coeffients
   pid = (Kp * p) + (Ki * i) + (Kd * d);
 
-  
 
   //only move motor if enabled switch is on
-  if(digitalRead(enableMotorSwitch) == HIGH){
-    
-  }  
+  if(enableMotor){
+    setServoSpeed(pid);
+  }else{
+    setServoSpeed(0);
+  }
 
+  //debug print twice a second
+  if(millis() >= now){
+    //debug();
+    now += 500;
+  }
 
-  //temp testing servo and encoder
-  setServoSpeed(setPosition / 512.0);
-  //Serial.println(readAnalogAngle(currPositionEncoder, 1));
-  debug();
-
-
-
+  //loop 100 times per second
+  delay(10);
 }
 
-//reads the current angle (0 to 360) of the analog encoder on the given pin
+//reads the current angle (-180 to 180) of the analog encoder on the given pin
 //takes n samples and averages the output
-float readAnalogAngle(int pin, int numSamples){
+float readAnalogAngle(int pin, float lastVal, float weight){
   const float minVoltage = 0.015;
   const float maxVoltage = 4.987;
-  int samples[numSamples]; //0-1023
 
-  //take samples
-  for(int i=0; i<numSamples; i++){
-    samples[i] = analogRead(pin); //0-1023
-  }
+  float raw = analogRead(pin) / 1024.0f * 5.0f;
+  float angle = (raw/maxVoltage * 360) - 180;
+  
+  float newVal = weight * lastVal + (1 - weight) * angle; 
 
-  //find average
-  float sum = 0.0;
-  for(int i=0; i<numSamples; i++){
-    sum += samples[i];
-  }
-  float voltage = (sum / numSamples)/1024 * 5.0; 
+  return newVal;
+}
 
-  //convert from voltage to angle
-  return (voltage/maxVoltage * 360) - 180;
+//reads an analog value and weights it against its previous value
+//this smooths out the signal
+float analogReadFiltered(int pin, float lastVal, float weight){
+  int raw = analogRead(pin);
+  float newVal = weight * lastVal + (1 - weight) * raw;
+  return newVal;
 }
 
 /*
  * sets the servo speed, -1 for full reverse, 1 for full forward
  */
 void setServoSpeed(float s){
+  s = constrain(s, -1.0, 1.0);
   servo.writeMicroseconds(minPWM + (s+1)/2.0 * (maxPWM - minPWM));
 }
 
 //print out all important values
 void debug(){
-  /*
-  Serial.print("error = " + error);
-  Serial.print("    ");
-  Serial.print("Kp = " + Kp);
-  Serial.print("    ");
-  Serial.print("Ki = " + Ki);
-  Serial.print("    ");
-  Serial.print("Kd = " + Kd);
-  Serial.print("    ");
-  Serial.print("pid = " + pid);
-  Serial.print("    ");
-  Serial.print("p = " + p);
-  Serial.print("    ");
-  Serial.print("i = " + i);
-  Serial.print("    ");
-  Serial.print("d = " + d);
-  Serial.println(" ");
-  */
+
+  //-----------------inputs-----------------
 
   Serial.print("Set pos: ");
-  Serial.print(setPosition);
+  Serial.print(setPos);
+  Serial.print("    ");
+
+  Serial.print("curr pos: ");
+  Serial.print(currPos);
   Serial.print("    ");
   
   Serial.print("pIn: ");
   Serial.print(pIn);
   Serial.print("    ");
-  
+
   Serial.print("iIn: ");
   Serial.print(iIn);
   Serial.print("    ");
-  
+
   Serial.print("dIn: ");
   Serial.print(dIn);
   Serial.print("    ");
-  
-  Serial.print("invert motor: ");
-  Serial.print(digitalRead(invertSetPositionSwitch) == HIGH);
+
+  Serial.print("invert set pos: ");
+  Serial.print(invertSetPos);
   Serial.print("    "); 
   
   Serial.print("enable motor: ");
-  Serial.print(digitalRead(enableMotorSwitch) == HIGH);
+  Serial.print(enableMotor);
   Serial.print("    "); 
+  
+
+  //-----------------coeffients-----------------
+
+  Serial.print("Kp: ");
+  Serial.print(Kp, 4);
+  Serial.print("    ");
+
+  Serial.print("Ki: ");
+  Serial.print(Ki, 4);
+  Serial.print("    ");
+
+  Serial.print("Kd: ");
+  Serial.print(Kd, 4);
+  Serial.print("    ");
+
+  //-----------------calculations-----------------
+
+  Serial.print("error: ");
+  Serial.print(error, 4);
+  Serial.print("    ");
+  
+  Serial.print("p: ");
+  Serial.print(p, 4);
+  Serial.print("    ");
+
+  Serial.print("i: ");
+  Serial.print(i, 4);
+  Serial.print("    ");
+
+  Serial.print("d: ");
+  Serial.print(d, 4);
+  Serial.print("    ");
+
+  Serial.print("pid: ");
+  Serial.print(pid, 4);
+  Serial.print("    ");
 
   Serial.println();
 
