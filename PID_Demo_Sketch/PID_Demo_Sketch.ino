2bbc811fb2ca6df830ec43ba597e84c4d332ac8d
crglick <crglick@iastate.edu>
2021-07-31 16:32:28 -0500

    tweaked coefficient scaling and added comments

--- PID_Demo_Sketch/PID_Demo_Sketch.ino
+++ PID_Demo_Sketch/PID_Demo_Sketch.ino
@@ -1,47 +1,66 @@
 #include <Servo.h>
 
-//Constants
-const int setPosPot = A3;
-const int currPosEncoder = A4;
+//----------Constants----------
+
+//defines where the inputs are plugged in
 const int Ppot = A0;
 const int Ipot = A1;
 const int Dpot = A2;
+const int setPosPot = A3;
+const int currPosEncoder = A4;
 
+//digital inputs
 const int invertSetPosSwitch = 3;
 const int enableMotorSwitch = 4;
-const int servoPin = 5;
-
 const int pEnablePin = 6;
 const int iEnablePin = 7;
 const int dEnablePin = 8;
 
+//servo output pin 
+const int servoPin = 5;
+
 const int maxPWM = 1750;  //in microseconds, the duty cycle PWM range to control speed
 const int minPWM = 1250;  //this is specific to the HSR-1425CR servo, tweak as needed for others
 
+Servo servo;    //output motor
+
 //calculation variables
 float setPos, currPos, pIn, iIn, dIn = 0.0f;   //analog inputs
-bool enableMotor, invertSetPos, pEnable, iEnable, dEnable = false;     //switch inputs
-float error, lastError, pid = 0.0f;    //TODO: label
+float lastSetPos, lastCurrPos, lastPIn, lastIIn, lastDIn = 0.0f; //last inputs for smoothing
+
+bool enableMotor, invertSetPos, pEnable, iEnable, dEnable = false;     //digital inputs
+float error, lastError;    //error is the difference between the current position, and the set point
 float p, i, d = 0.0f;       //values for p, i, and d factors
-float Kp, Ki, Kd = 0.0f;    //coefficents 
+float Kp, Ki, Kd = 0.0f;    //coefficients to scale the p, i, and d factors, these are used to tune the PID controller
+float pid = 0.0f;           //the final value passed to the motor to set the current speed
 
-//last interation of inputs for filtering
-float lastSetPos, lastCurrPos, lastPIn, lastIIn, lastDIn = 0.0f;
+long now = 0L;  //number of milliseconds since the arduino turned on
+                //for limiting number of calculations per second
 
-long now = 0L;
-
-Servo servo;
-
-/**
-   initialize arduino for operation
-*/
+/*
+ * initialize arduino for operation
+ */
 void setup() {
   Serial.begin(9600);   //start serial communication for console
   now = millis() + 500;
   
-  //input setup
+  //setup input for required analog
+  //and ON-OFF digital switches
   pinMode(invertSetPosSwitch, INPUT);
   pinMode(enableMotorSwitch, INPUT);
+
+  /*A pullup resistor is required for these switches because they are ON-OFF
+   *switches, where the other are ON-ON switches.
+   *the ON-OFF switches tie the pin to ground when in the on position and disconnects it from ground
+   *when in the off position. This state is called a *floating pin* and makes reading
+   *an input from the pin unreliable due to background noise.
+   *A pullup resistor is connected internally on the arduino to the 5v rail and this pin,
+   *letting only a small amount of current flow through it.
+   *With this, when the pin is not connected to ground, it is connected to 5v
+   *through the pullup resistor, making the off position (5v) much more stable for the switch.
+   *The ON-ON switch connects either ground or 5v to the pin and the floating
+   *pin issue never happens, so a pullup resistor is not needed.
+  */
   pinMode(pEnablePin, INPUT_PULLUP);
   pinMode(iEnablePin, INPUT_PULLUP);
   pinMode(dEnablePin, INPUT_PULLUP);
@@ -54,31 +73,31 @@ void setup() {
 /**
  * Main loop that controls the motor
  */
-void loop() {  
-  //set last values
+void loop() {    
+  //first read all the inputs into the system
+  pIn = analogReadSmoothed(Ppot, lastPIn, 0.9);   //range 0 to 1024
+  iIn = analogReadSmoothed(Ipot, lastIIn, 0.9);
+  dIn = analogReadSmoothed(Dpot, lastDIn, 0.9);
+  setPos = analogReadSmoothed(setPosPot, lastSetPos, 0.9);
+  currPos = readEncoderAngle(currPosEncoder, lastCurrPos, 0.9);
+
+  //set last values for next loop
   lastPIn = pIn;
   lastIIn = iIn;
   lastDIn = dIn;
   lastCurrPos = currPos;  
-  
-  //first read all the inputs into the system
-  pIn = analogReadFiltered(Ppot, lastPIn, 0.9);   //range 0 to 1024
-  iIn = analogReadFiltered(Ipot, lastIIn, 0.9);
-  dIn = analogReadFiltered(Dpot, lastDIn, 0.9);
-  setPos = analogReadFiltered(setPosPot, lastSetPos, 0.9);
-  
-  lastSetPos = setPos;  //TODO: clean this up, order things better and comment
+  lastSetPos = setPos;
 
-  //setPos = analogRead(setPosPot);
-  
+  //scale set position 
+  //normal range 0 to 1024, make range -180.0f to 180.0f
+  setPos = ((setPos / 1024.0f) * 360) - 180;
+
+  //read in digital inputs
   enableMotor = digitalRead(enableMotorSwitch) == HIGH;
   invertSetPos = digitalRead(invertSetPosSwitch) == HIGH;
-
-  //read set and current position 
-  //normal range int 0 to 1024, make range float -180 to 180
-  setPos = ((setPos / 1024.0) * 360) - 180;
-  currPos = readAnalogAngle(currPosEncoder, lastCurrPos, 0.9);
-
+  pEnable = digitalRead(pEnablePin) == HIGH;
+  iEnable = digitalRead(iEnablePin) == HIGH;
+  dEnable = digitalRead(dEnablePin) == HIGH;
 
   //invert setPos if needed
   if(invertSetPos){
@@ -90,41 +109,49 @@ void loop() {
     }
   }
   
-  //begin PID loop calculations
+  //--------------begin PID loop calculations--------------------
 
   //Scale the coefficients before they are passed in
   //from the input values (trial and error for scaling)
-  Kp = pIn / 4096.0f;     // 0 to 1
-  Ki = iIn / 16384.0f;     // 0 to 0.5
-  Kd = dIn / 4096.0f ;    // 0 to 1
+  //starts as 0 to 1023
   
-  error = currPos - setPos;    //error is degrees difference between where we want to be and where we are
+  //These coefficients are converting from an angle measurement (-180 to 180)
+  //to a motor power (-1 to 1). Because the scale of the two units are far apart
+  //a small number is required to be in a usable range.
+  //If the scale of the units were more similar, say from -5 to 5, to -1 to 1
+  //then a Kp closer to 1 would be used
+  
+  Kp = pIn / 1024.0f * 0.02f;    //based on motor speed input
+  Ki = iIn / 1024.0f * 0.001f;   //based on max i value (constrained later)
+  Kd = dIn / 1024.0f * 0.5f;     //slows the motor down as it approaches set point
+  
+  error = currPos - setPos;    //error is the degrees difference between where we want to be and where we are
 
   p = error;              //proportional is equal to the amount of error measured
 
   //i = i + error;         //integral accumulates the error each iteration
   
-  //Optional: create a dead band so the so integrel won't hunt back and fourth
-  if(abs(error) >  3) i = i + error;  // Integrate error if error > 1
-  if(error < 3.0 && error > -3.0) i = 0;               //Clear intergal if zero error
+  //Optional: create a dead band so the integrel won't hunt back and fourth
+  if(abs(error) >  1) i = i + error;              // accumulate error if error > 1
+  if(error < 1.0 && error > -1.0) i = 0;          //Clear intergal if between -1 and 1 error
 
-  i = constrain(i, -2000.0f, 2000.0f);      //stop i from increasing without bound
+  i = constrain(i, -10000.0f, 10000.0f);      //stop i from increasing without bound
 
   d = error - lastError;              //derivitive is the rate of change of the error
-  lastError = error;                  //save this error for the next loop
+  lastError = error;                  //save current error for the next loop calculation of d
 
-  //disable coefficents
-  if(digitalRead(pEnablePin) == LOW){
+  //disable coefficients
+  if(pEnable == false){
     Kp=0;
   }
-  if(digitalRead(iEnablePin) == LOW){
+  if(iEnable == false){
     Ki=0;
   }
-  if(digitalRead(dEnablePin) == LOW){
+  if(dEnable == false){
     Kd=0;
   }
 
-  //sum the PID factors with their coeffients
+  //sum the PID factors with their coefficients to get final motor speed
   pid = (Kp * p) + (Ki * i) + (Kd * d);
 
 
@@ -146,23 +173,29 @@ void loop() {
 }
 
 //reads the current angle (-180 to 180) of the analog encoder on the given pin
-//takes n samples and averages the output
-float readAnalogAngle(int pin, float lastVal, float weight){
-  const float minVoltage = 0.015;
+//takes in the last value from this function for smoothing
+//weight 0.0 to 1.0, 0.0=no smoothing, 1.0=output will not change from previous
+float readEncoderAngle(int pin, float lastVal, float weight){
+  const float minVoltage = 0.015;   //from encoder MA3-A10-250-N spec sheet
   const float maxVoltage = 4.987;
 
   float raw = analogRead(pin) / 1024.0f * 5.0f;
   float angle = (raw/maxVoltage * 360) - 180;
-  
+
+  //Smoothing function
   float newVal = weight * lastVal + (1 - weight) * angle; 
 
   return newVal;
 }
 
-//reads an analog value and weights it against its previous value
-//this smooths out the signal
-float analogReadFiltered(int pin, float lastVal, float weight){
+/*
+ * reads an analog value and weights it against its previous value
+ * this smooths out the signal
+ * 0.0 to 1.0, 0.0=no smoothing, 1.0=output will not change from previous
+ */
+float analogReadSmoothed(int pin, float lastVal, float weight){
   int raw = analogRead(pin);
+  //Smoothing function
   float newVal = weight * lastVal + (1 - weight) * raw;
   return newVal;
 }
@@ -175,7 +208,9 @@ void setServoSpeed(float s){
   servo.writeMicroseconds(minPWM + (s+1)/2.0 * (maxPWM - minPWM));
 }
 
-//print out all important values
+/*
+ * print out all important values 
+ */
 void debug(){
 
   //-----------------inputs-----------------
@@ -209,7 +244,7 @@ void debug(){
   Serial.print("    "); 
   
 
-  //-----------------coeffients-----------------
+  //-----------------coefficients-----------------
 
   Serial.print("Kp: ");
   Serial.print(Kp, 4);
